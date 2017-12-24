4d8dfe8f6d102be8dbd9d12914a2314ae85249ea
Maithu Venkatesh <maithuvenkatesh@Maithu.local>
2017-12-24 23:18:48 +0000

    Removing prod/dev config

--- beach/config.py
+++ beach/config.py
@@ -10,22 +10,16 @@ import os
 
 class Config(object):
     #Flask config
-    TESTING = False
+    TESTING = True
+    PORT = 8080
+    DEBUG = True
 
-    #DB config
+    DB config
     DB_SERVER = os.environ['DB_SERVER']
     DB_USERNAME = os.environ['DB_USERNAME']
     DB_PASSWORD = os.environ['DB_PASSWORD']
     DB_DATABASE = os.environ['DB_DATABASE']
     DB_PORT = int(os.environ['DB_PORT'])
 
+    
 
-
-class ProdConfig(Config):
-    PORT = 80
-    DEBUG = True
-
-
-class DevConfig(Config):
-    PORT = 8080
-    DEBUG = True
